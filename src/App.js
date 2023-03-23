926ebc9fd108de5b7b3a49d3a09595ce2dae2aa6
Chandan-repository <67368366+Chandan-repository@users.noreply.github.com>
2023-03-23 11:46:18 +0530

    MangingFormwith validation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import SimpleInput from './components/SimpleInput';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="app">
+      <SimpleInput />
     </div>
   );
 }
+
+export default App;
